8bb9e812da95270ed887ae35918c2cb0a625a2fb
wingfaijasonlaw <wingfaijasonlaw@hotmail.com>
2019-08-27 22:13:09 -0700

    Add movement animation
    Add directional movement animation.

--- Learning 2D/Assets/Scripts/PlayerMovement.cs
+++ Learning 2D/Assets/Scripts/PlayerMovement.cs
@@ -9,20 +9,35 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
+    private Animator animator;
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
-
+  
     // Update is called once per frame
     void Update()
     {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
 
